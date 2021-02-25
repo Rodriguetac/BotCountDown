3a0c9e139f873e17df47b0ff624fe25979c4ddf7
Rodriguetac <rodrigue.taccoen@viacesi.fr>
2021-02-25 20:26:45 +0100

    Add erwan command

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const Config = require('./Config.json');
+const Video = require('./Video.json');
 const ytdl = require('ytdl-core');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
@@ -17,29 +18,57 @@ bot.on('ready', () =>{
 bot.on('message', (message) =>{
     if (message.author.bot) return;
 
-    if (message.content.match(/^!countdown/))
+    if (message.content.match(/^\?countdown/))
     {
         if (message.member.voice?.channel)
         {
             message.member.voice.channel.join()
             .then(connection =>{
-                Play(connection)
+                Play(connection, Video.countdown)
             })
         }
         else
         {
-            message.reply("Please join an audio channel first");
+            message.reply("Please join an audio channel first")
+            .then(returnMessage =>{
+                returnMessage.delete({timeout: 3000});
+            });
         }
+    }
+    else if (message.content.match(/^\?help/))
+    {
+        message.channel.send(new Discord.MessageEmbed()
+                       .setTitle('Liste des commandes:')
+                       .addFields({ name: '?countdown', value:'Le bot rejoint votre channel audio et lance un compte à rebours', inline: true}))
+        .then(returnMessage =>{
+            returnMessage.delete({timeout:6000});
+        });
+        message.delete({timeout:5500})
+        .catch(err => console.log(err));
+    }
+    else if (message.content.match(/^\?erwan/))
+    {
+        message.member.voice.channel.join()
+            .then(connection =>{
+                Play(connection, Video.erwan)
+            })
     }
 })
 
-function Play(connection)
+function Play(connection, url)
 {
-    //let stream = ytdl('https://www.youtube.com/watch?v=zturQR5yJgA', { quality: 'highestaudio', filter: 'audioonly'});
-    let stream = ytdl('https://www.youtube.com/watch?v=KOoCEIwswYg', { quality: 'highestaudio', filter: 'audioonly'});
+    let stream;
+    if (url == Video.erwan)
+    {
+        stream = Video.erwan;
+    }
+    else{
+        stream = ytdl(url, { quality: 'highestaudio', filter: 'audioonly'});
+    }
+    
     const dispatcher = connection.play(stream, streamOptions);
 
-    dispatcher.on('finish', () =>{
+    dispatcher.on('finish', () => {
         connection.disconnect();
     });
 }
